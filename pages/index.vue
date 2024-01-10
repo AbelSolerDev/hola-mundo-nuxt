fe4d0a0e8d9a30188b7f37835012a0719a4177cf
Abel Soler <102069966+AbelSolerDev@users.noreply.github.com>
2024-01-10 14:58:04 +0100

    Con node y npm run dev todo funciona

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Hola {{ nombre }}</h1>
+    <form @submit.prevent="guardarNombre">
+      <label for="nombre">Nombre:</label>
+      <input type="text" id="nombre" v-model="nombreForm" />
+      <button type="submit">Guardar</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  data() {
+    return {
+      nombre: 'Mundo',
+      nombreForm: '',
+    };
+  },
+  methods: {
+    async guardarNombre() {
+      const response = await this.$axios.post('http://localhost:3000/guardarNombre', {
+        nombre: this.nombreForm,
+      });
+      console.log(response.data);
+      this.nombre = this.nombreForm;
+    },
+  },
+};
 </script>
