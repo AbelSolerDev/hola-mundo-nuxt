acba1ef6870f708fc6da516ceb5b751646e2230a
Abel Soler <102069966+AbelSolerDev@users.noreply.github.com>
2024-01-10 14:55:01 +0100

    primer commit con index.js creado

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', // Tu nombre de usuario de MySQL
+  password: '', // Tu contraseña de MySQL
+  database: 'hola_mundo_db',
+});
+
+app.use(express.json());
+
+app.post('/guardarNombre', (req, res) => {
+  const { nombre } = req.body;
+
+  connection.query('INSERT INTO nombres (nombre) VALUES (?)', [nombre], (err, results) => {
+    if (err) throw err;
+
+    res.json({ mensaje: `Nombre guardado: ${nombre}` });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor backend en http://localhost:${port}`);
+});
