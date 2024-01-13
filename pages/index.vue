18438109b4c669a03e5469abc1c23fd12fba283d
Abel Soler <102069966+AbelSolerDev@users.noreply.github.com>
2024-01-13 12:38:11 +0100

    funciona app de nuevo

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-    <h1>Hola {{ nombre }}</h1>
-    <form @submit.prevent="guardarNombre">
-      <label for="nombre">Nombre:</label>
-      <input type="text" id="nombre" v-model="nombreForm" />
-      <button type="submit">Guardar</button>
-    </form>
+    <div class="container">
+      <h1 class="titulo">Hola {{ nombre }}</h1>
+      <form @submit.prevent="guardarNombre" class="formulario">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" v-model="nombreForm" />
+        <button type="submit">Guardar</button>
+      </form>
+    </div>
   </div>
 </template>
 
+<style>
+.container {
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .titulo {
+    text-align: center;
+    font-size: 2em;
+  }
 
+  .formulario {
+    margin-top: 20px;
+    text-align: center;
+  }
+
+  body {
+    background-color: aquamarine;
+    padding: 20px;
+  }
+</style>
 
 
 <script>
